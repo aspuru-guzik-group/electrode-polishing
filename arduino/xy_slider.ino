4b1f669b81c3ded111d2748996a1869e2d7c346a
Gun Deniz Akkoc <g.akkoc@fz-juelich.de>
2023-07-18 23:30:12 +0200

    Now the slider takes commands in batches. The timing of the motors are improved. Corresponding Python code is converted to a device class with test function.

--- arduino/xy_slider.ino
+++ arduino/xy_slider.ino
@@ -1,156 +1,153 @@
-const char pulsePin0 = 2;
-const char pulsePin1 = 5;
-const char directPin0 = 3;
-const char directPin1 = 6;
-const int swPin0 = 8;
-const int swPin1 = 9;
+const char pulsePinX = 2; //x
+const char pulsePinY = 5; //y
+const char directPinX = 3;
+const char directPinY = 6;
+const int swPinX = 8;
+const int swPinY = 9;
 
-int readShort() {
-  // Read a short int from serial port, big endian. 
-  // Wait until two bytes from serial port
-  while (Serial.available() <= 1) {
-    continue;
-  }
-  // Read two bytes, assign to an int
-  unsigned char highByte = Serial.read();
-  unsigned char lowByte = Serial.read();
-  int a = highByte << 8 | lowByte;
-  return a;  
-}
+char jobType;
+const int inputSize = 60;
+byte inputBytes[inputSize+2];
 
-char readChar() {
-  while (Serial.available() < 1) {
-    continue;
+int16_t temp = 0;
+unsigned long xCount = 0;
+unsigned long yCount = 0;
+int xDir = 0;
+int yDir = 0;
+unsigned long xDelay = 0;
+unsigned long yDelay = 0;
+unsigned long runTime = 0;
+unsigned long nowMicros = 0;
+unsigned long xLastMicros = 0;
+unsigned long yLastMicros = 0;
+
+void processBatch() {
+  xLastMicros = micros();
+  yLastMicros = xLastMicros;
+  //read 4 bytes at a time from the buffer
+  //the first 2 bytes are for 16 bit signed int for x, the remaining 2 bytes are for y
+  //The sign is used for setting direction
+  for (int i = 0; i<inputSize-3; i=i+4) {
+    //for each command in the batch
+    temp = * (int16_t *) &inputBytes[i]; //the first 2 bytes
+    xDir = (temp > 0);
+    xCount = abs(temp);
+    
+    temp = * (int16_t *) &inputBytes[i+2]; //the next 2 bytes
+    yDir = (temp > 0);
+    yCount = abs(temp);
+    
+    //if x has more steps, set the total time by it (step count * 1000us), otherwise do the same for y
+    if (xCount > yCount) {
+      runTime = xCount * 1000;
+      xDelay = 1000;
+      yDelay = runTime / yCount;
+    }
+    else {
+      runTime = yCount * 1000;
+      yDelay = 1000;
+      xDelay = runTime / xCount;
+    }
+
+    takeSteps();
   }
-  char a = Serial.read();
-  return a;
 }
 
 void sendOnePulse(char pulsePin) {
-  switch (pulsePin) {
-    case 0:
-      digitalWrite(pulsePin0, LOW);
-      digitalWrite(pulsePin0, HIGH);
-      delayMicroseconds(5);
-      digitalWrite(pulsePin0, LOW);
-      delayMicroseconds(5);
-      break;
-    case 1:
-      digitalWrite(pulsePin1, LOW);
-      digitalWrite(pulsePin1, HIGH);
-      delayMicroseconds(5);
-      digitalWrite(pulsePin1, LOW);
-      delayMicroseconds(5);
-      break;
-    default:
-      break;
-  }
-}
-
-void sendPulses(int numPulse0, int numPulse1) {
-  int countPulse0 = 0;
-  int countPulse1 = 0;
-  long delayPulse0 = 100000 / numPulse0 - 10;
-  long delayPulse1 = 100000 / numPulse1 - 10;
-  while (countPulse0 < numPulse0 || countPulse1 < numPulse1) {
-    long timing0 = countPulse0 * (delayPulse0 + 10);
-    long timing1 = countPulse1 * (delayPulse1 + 10) +20;
-    if (timing0 + delayPulse0 < timing1) {
-      // If next pulse timing0 is less than timing1, send pulse0 and wait for delay between timing0s then next pulse0
-      sendOnePulse(0);
-      int deltaTime = delayPulse0;
-      delayMicroseconds(deltaTime);
-      countPulse0 += 1;
-    }
-    else if (timing0 <= timing1) {
-      // If pulse timing0 is less than timing1, send pulse0 and wait for timing1 - timing0 then pulse1
-      sendOnePulse(0);
-      int deltaTime = timing1 - timing0;
-      delayMicroseconds(deltaTime);
-      countPulse0 += 1;
-    }
-    else if (timing1 + delayPulse1 < timing0) {
-      // If next pulse timing1 is less than timing0, send pulse1 and wait for delay between timing1s then next pulse1
-      sendOnePulse(1);
-      int deltaTime = delayPulse1;
-      delayMicroseconds(deltaTime);
-      countPulse1 += 1;
-    }
-    else {
-      // Other cases send pulse1 and wait for timing0 - timing1 then pulse0
-      sendOnePulse(1);
-      int deltaTime = timing0 - timing1;
-      delayMicroseconds(deltaTime);
-      countPulse1 += 1;
-    }
-  }
-}
-
-void setOneDirect(char directPin, char level) {
-  if (directPin == 0) {
-    digitalWrite(directPin0, level);
-  } else if (directPin == 1) {
-    digitalWrite(directPin1, level);
-  }
-  delayMicroseconds(10);
+      digitalWrite(pulsePin, LOW);
+      digitalWrite(pulsePin, HIGH);
 }
 
 void resetPosition() {
-  // Go left until switch is pushed
-  setOneDirect(1, HIGH);
-  while (digitalRead(swPin1) == HIGH) {
-    sendOnePulse(1);
-    delayMicroseconds(1000);
-  }
-  // Go to the origin
-  setOneDirect(1, LOW);
-  for (int i=0; i<10000; i++) {
-    sendOnePulse(1);
-    delayMicroseconds(1000);
-  }
-
-  // Go down until switch is pushed
-  setOneDirect(0, HIGH);
-  while (digitalRead(swPin0) == HIGH) {
-    sendOnePulse(0);
-    delayMicroseconds(1000);
-  }
-  // Go to the origin
-  setOneDirect(0, LOW);
-  for (int i=0; i<10000; i++) {
-    sendOnePulse(0);
+  char xEnd = digitalRead(swPinX);
+  char yEnd = digitalRead(swPinY);
+  unsigned long xHomeCount = 10000;
+  unsigned long yHomeCount = 10000;
+  bool cont = true;
+  while (cont) {
+    cont = false;
+    if (xEnd == HIGH) {
+      digitalWrite(directPinX, HIGH);
+      sendOnePulse(pulsePinX);
+      xEnd = digitalRead(swPinX);
+      cont = true;
+    }
+    else if (xHomeCount > 0) {
+      digitalWrite(directPinX, LOW);
+      sendOnePulse(pulsePinX);
+      xHomeCount--;
+      cont = true;
+    }
+    
+    if (yEnd == HIGH) {
+      digitalWrite(directPinY, HIGH);
+      sendOnePulse(pulsePinY);
+      yEnd = digitalRead(swPinY);
+      cont = true;
+    }
+    else if (yHomeCount > 0) {
+      digitalWrite(directPinY, LOW);
+      sendOnePulse(pulsePinY);
+      yHomeCount--;
+      cont = true;
+    }
+    
     delayMicroseconds(1000);
   }
 }
 
+
+void takeSteps() {
+  digitalWrite(directPinX, xDir);
+  digitalWrite(directPinY, yDir);
+
+  //Loop over each pulse until there are no steps neither for x nor for y
+  while (xCount + yCount > 0) {
+    nowMicros = micros();
+    //check if motor x's time has come to pulse and if there are steps to be taken
+    if ((xCount > 0) && (nowMicros - xLastMicros > xDelay)) {
+      sendOnePulse(pulsePinX);
+      xCount--;
+      //Serial.println(xCount);
+      xLastMicros = nowMicros;
+    }
+    //check if motor y's time has come to pulse and if there are steps to be taken
+    if ((yCount > 0) && (nowMicros - yLastMicros > yDelay)) {
+      sendOnePulse(pulsePinY);
+      yCount--;
+      yLastMicros = nowMicros;
+    }
+  }
+}
+
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(pulsePin0, OUTPUT);
-  pinMode(pulsePin1, OUTPUT);
-  pinMode(directPin0, OUTPUT);
-  pinMode(directPin1, OUTPUT);
-  pinMode(swPin0, INPUT_PULLUP);
-  pinMode(swPin1, INPUT_PULLUP);
+  pinMode(pulsePinX, OUTPUT);
+  pinMode(pulsePinY, OUTPUT);
+  pinMode(directPinX, OUTPUT);
+  pinMode(directPinY, OUTPUT);
+  pinMode(swPinX, INPUT_PULLUP);
+  pinMode(swPinY, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  while (!Serial) {
-    continue;
-  }
-  char directMotor0 = readChar();
-  char directMotor1 = readChar();
-  int numPulseMotor0 = readShort();
-  int numPulseMotor1 = readShort();
-  if (directMotor0 == 2) {
-    resetPosition();
-    Serial.println("Reset done");
-  } else {
-    setOneDirect(0, directMotor0);
-    setOneDirect(1, directMotor1);
-    sendPulses(numPulseMotor0, numPulseMotor1);
-    Serial.println("[" + String((int)directMotor0) + ", " + String((int)directMotor1) + ", " 
-            + String(numPulseMotor0) + ", " + String(numPulseMotor1) + "]");
+  if (Serial.available() > 0) {
+    jobType = Serial.read();
+    switch (jobType) {
+      case 'R': //R for resetting-homing
+        resetPosition();
+        Serial.println("Homing Completed");
+        break;
+      case 'M': //M for movement
+        while (Serial.available() < inputSize){}
+        Serial.readBytes(inputBytes,inputSize);
+        processBatch();
+        Serial.println("Batch Completed");
+        break;
+      default: //unknown message
+        Serial.println("Unknown command received.");
+        break; 
+    }
   }
 }
